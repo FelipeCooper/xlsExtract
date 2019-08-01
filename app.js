e4acb1690df615b08bc5c750f8d6737f0bf2c7cf
Felipe Cooper <felipecardoso323@hotmail.com>
2019-08-01 14:34:11 -0300

    Inicio Projeto

---
+++ app.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var path = require('path')
+let pastaAtual = path.dirname(__dirname) + '/' + path.basename(__dirname)
+
+function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, files_);
+        } else {
+            if (path.extname(name) == '.xlsx' && path.basename(name).substr(0, 13) == 'Ficha_tecnica') {
+                files_.push(path.basename(name));
+            }
+        }
+    }
+    return files_;
+}
+(async e => {
+    var nameFiles = getFiles(pastaAtual)
+    var Excel = require('exceljs');
+    var fichatecnica = new Excel.Workbook();
+    var relatorio = new Excel.Workbook();
+    let dados = []
+    relatorioSheet = relatorio.addWorksheet('teste');
+    for (j = 0; j < nameFiles.length; j++) {
+        await fichatecnica.xlsx.readFile(nameFiles[j])
+        var fichaSheet = fichatecnica.getWorksheet("FICHA");
+        dados.push(Array(
+            fichaSheet.getCell('C5').value,
+            fichaSheet.getCell('C6').value,
+            fichaSheet.getCell('E3').value,
+            fichaSheet.getCell('G4').value,
+            fichaSheet.getCell('G5').value
+        ))
+    }
+    relatorioSheet.columns = [
+        { header: 'Sindico', key: 'sindico', width: 30 },
+        { header: 'Tipo', key: 'tipo', width: 18 },
+        { header: 'Cód', key: 'cod', width: 10 },
+
+    ];
+    relatorioSheet.addRows(dados)
+    relatorio.xlsx.writeFile('relatorio.xlsx');
+})()
